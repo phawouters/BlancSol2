6b3b63bb8ad45844e784dfb4035946658c3e9b64
Pieter Wouters <pieter_wouters@hotmail.com>
2026-08-09 14:52:09 +0200

    Update greeting text and button click message
    Replaced "Hello, dummy world!" with "Hello world!" in index.html. Updated Script3.js to set the greeting text to "You clicked the button!" on click.

--- Script3.js
+++ Script3.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var greeting = document.getElementById("greeting");
 
     button.addEventListener("click", function () {
-        greeting.textContent = "You clicked the dummy button!";
+        greeting.textContent = "You clicked the button!";
     });
 });
