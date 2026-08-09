a2cee1954c3029eb632256133b33e8921f3eaa2d
Pieter Wouters <pieter_wouters@hotmail.com>
2026-08-09 13:02:07 +0200

    Add project files.

---
+++ Script3.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.getElementById("clickMe");
+    var greeting = document.getElementById("greeting");
+
+    button.addEventListener("click", function () {
+        greeting.textContent = "You clicked the dummy button!";
+    });
+});
